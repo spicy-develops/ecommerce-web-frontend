c8b10f499cf542d46007b82465f96f0088ab0650
SpicyTalha <talhamushtaq73@gmail.com>
2024-06-09 14:21:12 +0500

    Created Home Wrapper 1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,72 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <section className="home-wrapper-1 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <div className="main-banner position-relative">
+                <img src="images/main-banner-1.jpg" className="img-fluid rounded-3" alt="main banner" />
+                <div className="main-banner-content position-absolute">
+                  <h4>DO WHAT YOU CAN'T</h4>
+                  <h5>Galaxy 24 Ultra</h5>
+                  <p>From Rs.450000 or Rs.50000/mo</p>
+                  <Link className='button'>BUY NOW</Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="d-flex flex-wrap gap-10 justify-content-between align-items-center">
+                <div className="small-banner position-relative">
+                  <img src="images/catbanner-01.jpg" className="img-fluid rounded-3" alt="small banner" />
+                  <div className="small-banner-content position-absolute">
+                    <h4>NEW ARRIVAL</h4>
+                    <h5>Galaxy 24 Ultra</h5>
+                    <p>From Rs.9000</p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative">
+                  <img src="images/catbanner-02.jpg" className="img-fluid rounded-3" alt="small banner" />
+                  <div className="small-banner-content position-absolute">
+                    <h4>15% OFF</h4>
+                    <h5>Galaxy 24 Ultra</h5>
+                    <p>From Rs.50000</p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative">
+                  <img src="images/catbanner-03.jpg" className="img-fluid rounded-3" alt="small banner" />
+                  <div className="small-banner-content position-absolute">
+                    <h4>NEW ARRIVAL</h4>
+                    <h5>Galaxy Tab A7</h5>
+                    <p>From Rs.39000</p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative">
+                  <img src="images/catbanner-04.jpg" className="img-fluid rounded-3" alt="small banner" />
+                  <div className="small-banner-content position-absolute">
+                    <h4>NEW ARRIVAL</h4>
+                    <h5>Audionic A-110</h5>
+                    <p>From Rs.4000</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
